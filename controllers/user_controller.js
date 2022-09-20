2f2a5c62a0c62d63c0646aa2ac59139296c7fb2d
JJieunn <lancia1228@gamil.com>
2022-09-20 17:32:43 +0900

    임시 저장

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,8 +1,21 @@
 const userService = require('../services/user_service');
 
-const signUpController = async (req, res) => {
+const logInController = async (req, res) => {
+  const { id, pw } = req.body;
 
+  if(!(id && pw)) {
+    res.status(400).json({ error: "INPUT_ERROR"})
+    return;
+  }
+
+  try{
+    const resData = await userService.logInService(id, pw)
+    res.status(201).json({ message: "LOGIN_SUCCESS", data: resData })
+  } catch (error) {
+    console.log(error);
+    res.status( error.statusCode || 500 ).json({ error: error.message })    
+  }
 }
 
 
-module.exports = { signUpController };
+module.exports = { logInController };
--- services/user_service.js
+++ services/user_service.js
@@ -1,7 +1,33 @@
 const userDao = require('../models/user_dao');
+const jsonwebtoken = require('jsonwebtoken')
+const bcrypt = require('bcryptjs')
 
-const signUpService = async () => {
 
+const logInService = async (email, password) => {
+    const userEmailPw = await userDao.getUserByEmail(email);
+    if (!userEmailPw) {
+      const error = new Error('USER_NOT_EXISTE');
+      error.statusCode = 400;
+      throw error;
+    }
+  
+    const isPasswordCorrect = bcrypt.compareSync(password, userEmailPw.password);
+  
+    if (!isPasswordCorrect) {
+      const error = new Error('PASSWORD_INCORRECTED');
+      error.statusCode = 400;
+      throw error;
+    } else if (isPasswordCorrect) {
+      const token = jwt.sign({ userEmail: userEmailPw.email }, SECRET_KEY, {
+        expiresIn: '1d',
+      });
+  
+    //   const user = {};
+    //   user["account"] = userIdPw["account"]
+    //   user["token"] = token
+    //   return user;
+    }
+  
 }
 
-module.exports = { signUpService }
+module.exports = { logInService }
